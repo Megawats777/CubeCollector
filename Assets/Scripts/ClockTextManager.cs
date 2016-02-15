569da8c45e12208422fd3d99667dc9d32fbc5425
Megawats777 <jedsemilla@gmail.com>
2016-02-15 12:30:13 -0500

    Adjusted color and content of ClockText

--- Assets/Scripts/ClockTextManager.cs
+++ Assets/Scripts/ClockTextManager.cs
@@ -22,8 +22,18 @@ public class ClockTextManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        // Set the content of the text component
+        setTextContent();
+
+        // Set the color of the text component
+        setTextColour();
+	}
+
+    // Set the content of the text
+    private void setTextContent()
+    {
         // Update the content of clockText
-        clockText.text = "Time: " + (int)gameManagerRef.clocklength;
+        clockText.text = "" + (int)gameManagerRef.clocklength;
 
         // If the game clock reaches zero
         if (gameManagerRef.clocklength < 1)
@@ -34,5 +44,19 @@ public class ClockTextManager : MonoBehaviour {
         {
             clockText.text = null;
         }
-	}
+    }
+
+    // Set the color of the text
+    private void setTextColour()
+    {
+        if (gameManagerRef.clocklength <= 6)
+        {
+            clockText.color = Color.red;
+        }
+        else
+        {
+            clockText.color = Color.white;
+        }
+    }
+
 }
