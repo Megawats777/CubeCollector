3bf3a005dca9ff3663aa1edc3cef1d16d376c29a
Megawats777 <jedsemilla@gmail.com>
2016-02-20 12:37:03 -0500

    Added exitTextManagerClass
    This class manages the state of the exit message throughout the game

---
+++ Assets/Scripts/ExitTextManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ExitTextManager : MonoBehaviour
+{
+    // Reference to the text component
+    [HideInInspector]
+    public Text exitText;
+
+    // Reference to the gameManager class
+    [HideInInspector]
+    public gameManager gameManagerRef;
+
+    // Content of the exitText component
+    public string exitTextContent = "Press Esc to Exit";
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Get the text component
+        exitText = GetComponent<Text>();
+
+        // Get the gameManager class
+        gameManagerRef = FindObjectOfType<gameManager>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Set the content of the text component
+        setTextContent();
+	}
+
+    // Set the content of the text component
+    private void setTextContent()
+    {
+        // If there are not pickups left in the level display the exit message
+        if (gameManagerRef.pickUpAmount == 0)
+        {
+            exitText.text = exitTextContent;
+        }
+        // Otherwise set the content to null
+        else
+        {
+            exitText.text = null;
+        }
+    }
+
+
+
+}
