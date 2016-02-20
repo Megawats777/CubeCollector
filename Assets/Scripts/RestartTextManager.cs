7f03d1334fdc09ff139ff197f1f2ff44d3f418e2
Megawats777 <jedsemilla@gmail.com>
2016-02-20 12:24:47 -0500

    Assigned the content of restartText to a string variable

---
+++ Assets/Scripts/RestartTextManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RestartTextManager : MonoBehaviour
+{
+    // Reference to the text component
+    [HideInInspector]
+    public Text restartText;
+
+    // Reference to the gameManager class
+    [HideInInspector]
+    public gameManager gameManagerRef;
+
+    // Content for the restartText component
+    public string restartTextContent = "Press Space to restart game";
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Get the text component
+        restartText = GetComponent<Text>();
+
+        // Get the gameManager
+        gameManagerRef = FindObjectOfType<gameManager>();
+
+        // Set the content of the text component to null
+        restartText.text = null;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Set text component content
+        setTextContent();
+
+	}
+
+    // Set text component content
+    private void setTextContent()
+    {
+        // If all pickups were collected then set the content to notify the player they can restart the game
+        if (gameManagerRef.pickUpAmount == 0)
+        {
+            restartText.text = restartTextContent;
+        }
+        // Otherwise set the content of the text component to null
+        else
+        {
+            restartText.text = null;
+        }
+    }
+}
