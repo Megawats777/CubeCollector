48add30681dda1890308779b51acff9b2f9f9a55
Megawats777 <jedsemilla@gmail.com>
2016-02-21 20:24:12 -0500

    Added menu navigation

---
+++ Assets/Scripts/MainMenuNavigator.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuNavigator : MonoBehaviour
+{
+    // Level names
+    public string[] levelNames;
+
+    // Can the user select a level
+    [HideInInspector]
+    public bool canSelectLevel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        canSelectLevel = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Manage level selection
+        selectLevel();
+
+        // Exit the game
+        exitGame();
+	}
+
+    // Level selection 
+    private void selectLevel()
+    {
+        // Select level one
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // If level One exists
+            if (levelNames[0] != null)
+            {
+                canSelectLevel = false;
+                SceneManager.LoadSceneAsync(levelNames[0]);
+            }
+        }
+
+
+
+    }
+
+    // Exit the game
+    private void exitGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
