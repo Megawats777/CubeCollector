134ba723616f70cb0115f2db7dc82936c569532a
Megawats777 <jedsemilla@gmail.com>
2016-03-31 11:49:57 -0400

    Started work on allowing scaling the object in y axis

--- Assets/Scripts/ScalingCube.cs
+++ Assets/Scripts/ScalingCube.cs
@@ -5,6 +5,14 @@ public class ScalingCube : MonoBehaviour {
 
     /*---Variables---*/
     private float newScaleX = 0;
+    private float newScaleY = 0;
+
+    /*---Determine if the object can scale in a paticular axis---*/
+    [SerializeField]
+    private bool canScaleInXAxis = true;
+
+    [SerializeField]
+    private bool canScaleInYAxis = false;
 
     /*---The rate the object will change size---*/
     [Range(0.0f, 1.0f), SerializeField]
@@ -12,8 +20,14 @@ public class ScalingCube : MonoBehaviour {
 
     /*---The Dimensions of the scaled object---*/
     private float smallestXSize = 0;
+    private float smallestYSize = 1;
 
+    [Range(0.0f, 15.0f), SerializeField]
     private float largestXSize = 0.2f;
+
+    [Range(0.0f, 15.0f), SerializeField]
+    private float largestYSize = 0.2f;
+
 
     [SerializeField]
     private float scaleLerpValue = 0.0001f;
@@ -26,6 +40,9 @@ public class ScalingCube : MonoBehaviour {
     {
         // Set the default x axis scale of the object
         smallestXSize = transform.localScale.x;
+
+        // Set the default y axis of the object
+        smallestYSize = transform.localScale.y;
 	}
 	
 	// Update is called once per frame
@@ -53,12 +70,33 @@ public class ScalingCube : MonoBehaviour {
             scaleLerpValue = Mathf.Clamp(scaleLerpValue - 0.01f * scalingRate, 0, 1);
         }
 
-        // Update the scale lerp value
-        newScaleX = Mathf.Lerp(smallestXSize, largestXSize, scaleLerpValue);
+        if (canScaleInXAxis == true)
+        {
+            // Update the scale lerp value for the X axis
+            newScaleX = Mathf.Lerp(smallestXSize, largestXSize, scaleLerpValue);
+        }
 
-        // Set the scale properties of the gameobject
-        transform.localScale = new Vector3(newScaleX, transform.localScale.y, transform.localScale.z);
+        if (canScaleInYAxis == true)
+        {
+            // Update the scale lerp value for the Y axis
+            newScaleY = Mathf.Lerp(smallestYSize, largestYSize, scaleLerpValue);
+        }
 
+        // Set the scale of the object
+        transform.localScale = new Vector3(newScaleX, newScaleY, transform.localScale.z);
+
+        // Change scaling mode for the X axis of the object
+        changeXAxisScaleMode();
+
+        // Change scaling mode for the Y axis of the object
+        //changeYAxisScaleMode();
+
+    }
+
+    // Change scaling mode for the X axis of the object
+    private void changeXAxisScaleMode()
+    {
+        // If the x axis of the object reaches its limits change the scaling mode
         if (transform.localScale.x == largestXSize)
         {
             isGrowing = false;
@@ -73,8 +111,29 @@ public class ScalingCube : MonoBehaviour {
 
             transform.localScale += new Vector3(0.01f, 0.0f, 0.0f);
         }
-        
     }
+
+    // Change scaling mode for the Y axis of the object
+    private void changeYAxisScaleMode()
+    {
+        // If the y axis of the object reaches its limits change the scaling mode
+        if (transform.localScale.y == largestYSize)
+        {
+            isGrowing = false;
+            isShrinking = true;
+
+            transform.localScale -= new Vector3(0.0f, 0.01f, 0.0f);
+        }
+        else if (transform.localScale.y == smallestYSize)
+        {
+            isGrowing = true;
+            isShrinking = false;
+
+            transform.localScale += new Vector3(0.0f, 0.01f, 0.0f);
+        }
+    }
+
+
 
     // Change the scaling mode
     IEnumerator changeScaleMode()
