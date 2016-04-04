42185c0e203ea45d8c4df5ac731b39082a57f4e8
Megawats777 <jedsemilla@gmail.com>
2016-04-03 21:04:19 -0400

    Created functionality for the sliding cube
    Details: The cube can move back and forth from it's original location to
    a navigation point attatched to it

--- Assets/Scripts/SlidingCube.cs
+++ Assets/Scripts/SlidingCube.cs
@@ -3,14 +3,137 @@ using System.Collections;
 
 public class SlidingCube : MonoBehaviour
 {
+    /*---Variables---*/
+
+    /*
+    // Determine if the object can move in a paticular axis
+    [SerializeField]
+    private bool canMoveInX = true;
+
+    [SerializeField]
+    private bool canMoveInY = false;
+    */
+
+    // Set which direction is the object moving in
+    private bool isMovingToNavPoint = true;
+    private bool isMovingToOrigin = false;
+
+    // The rate at which the object is moving
+    [SerializeField, Range(0.0f, 3.0f)]
+    private float movementSpeed = 0.5f;
+
+    // The lerp value of the location
+    [SerializeField]
+    private float positionLerpValue = 0.0001f;
+
+    // The new locations of the object per axis
+    private float newLocationX = 0.0f;
+    private float newLocationY = 0.0f;
+    private float newLocationZ = 0.0f;
+
+    // Reference to the navPoint
+    SlidingCubeMovePoint navPointRef;
+
+    // Reference to the location of the navPoint
+    [SerializeField]
+    private Vector3 navPointLocation = new Vector3(0, 0, 0);
+
+    // The origin location
+    private Vector3 originLocation;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        // Get the SlidingCubeMovePoint
+        navPointRef = GetComponentInChildren<SlidingCubeMovePoint>();
+
+        // Set the origin location
+        originLocation = transform.position;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        // Get the location of the nav point
+        navPointLocation = navPointRef.navPointLocation;
+
+        // Perform the object's movement
+        objectMovement();
+    }
+
+    // Perform the object's movement
+    private void objectMovement()
+    {
+        // Set the position lerp value
+        setPositionLerpValue();
+
+        // Set the object's location
+        setObjectLocation();
+
+        // Set the object's moving direction
+        setMovementDirection();
+    }
+
+    // Set the position lerp value
+    private void setPositionLerpValue()
+    {
+        // If the object is moving to the nav point increase the lerp value
+        if (isMovingToNavPoint == true && isMovingToOrigin == false)
+        {
+            Mathf.Clamp(positionLerpValue += 0.01f * movementSpeed, 0.0f, 1.0f);
+        }
+
+        // If the object is moving to the origin point decrease the lerp value
+        else if (isMovingToNavPoint == false && isMovingToOrigin == true)
+        {
+            Mathf.Clamp(positionLerpValue -= 0.01f * movementSpeed, 0.0f, 1.0f);
+        }
+    }
+
+    // Set the object's location
+    private void setObjectLocation()
+    {
+        // Blend the object's location in the X axis
+        newLocationX = Mathf.Lerp(originLocation.x, navPointLocation.x, positionLerpValue);
+
+        // Blend the object's location in the Y axis
+        newLocationY = Mathf.Lerp(originLocation.y, navPointLocation.y, positionLerpValue);
+
+        // Blend the object's location in the Z axis
+        newLocationZ = Mathf.Lerp(originLocation.z, navPointLocation.z, positionLerpValue);
+
+        // Set the new object location
+        transform.position = new Vector3(newLocationX, newLocationY, newLocationZ);
+    }
+
+    // Set the object's moving direction
+    private void setMovementDirection()
+    {
+        // If the location of the object is equal to the location of the nav point move back to the origin point
+        if (transform.position == navPointLocation)
+        {
+            isMovingToNavPoint = false;
+            isMovingToOrigin = true;
+
+            // Decrease the lerp value a little bit
+            positionLerpValue -= 0.00001f;
+
+            print("Moving to origin");
+        }
+        
+        // If the location of the object is equal to the location of the origin point move to the nav point 
+        else if (transform.position == originLocation)
+        {
+            isMovingToNavPoint = true;
+            isMovingToOrigin = false;
+
+            // Increase the lerp value a little bit
+            positionLerpValue += 0.00001f;
+            print("Moving to nav point");
+        }
+    }
+
+    
+
 }
--- Assets/Scripts/SlidingCubeMovePoint.cs
+++ Assets/Scripts/SlidingCubeMovePoint.cs
@@ -3,16 +3,33 @@ using System.Collections;
 
 public class SlidingCubeMovePoint : MonoBehaviour
 {
+    /*----Variables----*/
+
+    // Reference to the mesh renderer
+    MeshRenderer navPointMesh;
+
+    // The location of the navPoint
+    [HideInInspector]
+    public Vector3 navPointLocation = new Vector3(0, 0, 0);
 
 	// Use this for initialization
 	void Start ()
     {
-	
+        // Get the navPointMesh mesh renderer
+        navPointMesh = GetComponent<MeshRenderer>();
+
+        // Disable the navPointMesh mesh renderer
+        navPointMesh.enabled = false;
+
+        // Set the value of the nav point location
+        navPointLocation = transform.position;
+
+        print("The location of the nav point is " + navPointLocation.ToString());
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+        
+    }
 }
