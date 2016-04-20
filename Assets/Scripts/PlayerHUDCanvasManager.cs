78f1ba561ed42216a3e145aeec8eb98fd4d9a287
Megawats777 <jedsemilla@gmail.com>
2016-04-20 17:52:20 -0400

    Added manager for the player hud canvas
    this allows for the player hud canvas to automatically activate when the
    game begins

---
+++ Assets/Scripts/PlayerHUDCanvasManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHUDCanvasManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
