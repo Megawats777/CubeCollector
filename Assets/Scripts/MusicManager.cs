94af4dd2455c91cfcef3661fdc28e84ab51c8627
Megawats777 <jedsemilla@gmail.com>
2016-04-24 12:58:46 -0400

    Added to music library

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -11,40 +11,66 @@ public class MusicManager : MonoBehaviour
     [SerializeField]
     private AudioClip[] musicClips;
 
+    // A number to decide which song to play
     int indexNum = 0;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         // Get the audio source
         soundSource = GetComponent<AudioSource>();
         soundSource.playOnAwake = true;
 
-        setMusicClip();
+        // Set a song index number and set the audio clip to be played
+        setIndexNum();
 
-        InvokeRepeating("setMusicClip", musicClips[indexNum].length + 0.5f, 1.0f);
-        print(indexNum);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-	
-	}
+        // Check if a song exists in the selected field
+        songCheck();
 
-    // Set the sound to play
-    private void setMusicClip()
-    {
-        soundSource.clip = musicClips[getNewNum()];
+        // Play a song based on the song index number
         soundSource.Play();
+
+        // Start the playing the set of songs
+        InvokeRepeating("playSong", 0.0f, musicClips[indexNum].length + 0.5f);
     }
 
-    // Generate a random number to select a song
-    private int getNewNum()
+    // Update is called once per frame
+    void Update()
     {
-        indexNum = Random.Range(0, 1);
 
-        return indexNum;
+    }
+
+    // Set a song index number and set the audio clip to be played
+    private void setIndexNum()
+    {
+        indexNum = Random.Range(0, musicClips.Length);
+        soundSource.clip = musicClips[indexNum];
+    }
+
+    // Play a song based on the song index number
+    private void playSong()
+    {
+        print("Music Changing");
+
+        // Set a song index number and set the audio clip to be played
+        setIndexNum();
+
+        // Check if a song exists in the selected field
+        songCheck();
+    }
+
+    // Check if a song exists in the selected field
+    private void songCheck()
+    {
+        if (musicClips[indexNum])
+        {
+            // Play the song
+            soundSource.Play();
+        }
+        else
+        {
+            print("No song selected in this field");
+        }
     }
 
 }
