b972c6b1676cf1e663280e88cfa95cf5098811a3
Megawats777 <jedsemilla@gmail.com>
2016-04-24 09:39:05 -0400

    Added music manager class

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+    // Reference to the audio source component
+    private AudioSource soundSource;
+
+    // A list of music
+    [SerializeField]
+    private AudioClip[] musicClips;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Get the audio source
+        soundSource = GetComponent<AudioSource>();
+        soundSource.playOnAwake = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    // Set the sound to play
+}
